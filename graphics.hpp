68c50ca9a3ea67ad59a55203085d38446ac4c6f8
Matthew <Bean6754@users.noreply.github.com>
2018-07-28 23:34:56 +0100

    Delete graphics.hpp

--- graphics.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef YOUR_NAME_INCLUDE
-#define YOUR_NAME_INCLUDE
-
-/* Your function statement here */
-std::string G_smile = "=]";
-std::string G_smilebig = "{0} {0}\n   w\n \\___/";
-std::string G_shocked = "=o";
-std::string G_shockedbig = "{0} {0}\n   w\n   O";
-#endif
