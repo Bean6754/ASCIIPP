4ba1d93c2b99cae3e3a2efe69e6a25d216b12908
Matthew <Bean6754@users.noreply.github.com>
2018-07-28 23:32:50 +0100

    Create example.cpp

---
+++ example.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#include "graphics.h"
+
+int main(void)
+{
+	std::cout << "Smile:\n" << G_smile << "\n" << std::endl;
+	std::cout << "Smile big:\n" << G_smilebig << "\n" << std::endl;
+	std::cout << "Shocked:\n" << G_shocked << "\n" << std::endl;
+	std::cout << "Shocked big:\n" << G_shockedbig << "\n" << std::endl;
+
+	return 0;
+}
